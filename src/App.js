2859363fba23a00c44fc00631ad426aa66f2f661
oojas <oojasgupta@gmail.com>
2023-07-08 12:34:21 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
@@ -18,7 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Navbar></Navbar>
     </div>
+   
   );
 }
 
